2760a54c2d7aebdbd0913c90833a5a410e055937
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-08-31 21:32:08 -0300

    :sparkles: add up&down stream functions

--- images.go
+++ images.go
@@ -2,21 +2,68 @@ package imageLoaderAPI
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	images "image-loader/gen/images"
 	"image-loader/mongo"
+	"io/ioutil"
 	"log"
 )
 
 type imagesrvc struct {
-	logger          *log.Logger
-	imageRepository mongo.ImageRepository
+	logger    *log.Logger
+	imagesDao mongo.ImageRepository
 }
 
 //NewImageLoader .
-func NewImageLoader(logger *log.Logger, imageRepository mongo.ImageRepository) images.Service {
-	return &imagesrvc{logger, imageRepository}
+func NewImageLoader(logger *log.Logger, imagesDao mongo.ImageRepository) images.Service {
+	return &imagesrvc{logger, imagesDao}
 }
 
-func (s *imagesrvc) LoadNewSatelliteImage(ctx context.Context, payload *images.SatelliteImage) (err error) {
+func (s *imagesrvc) LoadNewSatelliteImage(ctx context.Context, payload *images.RawSatelliteImage) (result *images.GoaResult, err error) {
+
+	filename := *payload.FileName
+	if filename == "" {
+		return nil, images.MakeBadRequest(errors.New("File name cannot be empty"))
+	}
+
+	originalFile, err := ioutil.ReadFile(filename)
+	//error reading original file
+	if err != nil {
+		return nil, images.MakeBadRequest(err)
+	}
+
+	bytesWritten, err := s.imagesDao.AddRawImage(originalFile, filename)
+	//error adding image to database
+	if err != nil {
+		return nil, images.MakeErrorAddingImage(err)
+	}
+
+	//everything went ok
+	code := "200"
+	description := fmt.Sprintf("Bytes written %d", bytesWritten)
+	result = &images.GoaResult{Code: &code, Description: &description}
+	return result, nil
+}
+
+func (s *imagesrvc) GetRawSatelliteImage(ctx context.Context, payload *images.GetRawSatelliteImagePayload) (result *images.GoaResult, err error) {
+	filename := *payload.FileName
+	if filename == "" {
+		return nil, images.MakeBadRequest(errors.New("File name cannot be empty"))
+	}
+
+	bytesRead, err := s.imagesDao.GetRawImage(filename)
+
+	if err != nil {
+		return nil, images.MakeErrorGettingImage(err)
+	}
+
+	code := "200"
+	description := fmt.Sprintf("Bytes read %d", bytesRead)
+	result = &images.GoaResult{Code: &code, Description: &description}
+	return result, nil
+}
+
+func (s *imagesrvc) LoadNewProcessedSatelliteImage(ctx context.Context, payload *images.ProcessedSatelliteImage) (err error) {
 	return nil
 }
--- mongo/images_dao.go
+++ mongo/images_dao.go
@@ -1,9 +1,14 @@
 package mongo
 
 import (
+	"bytes"
 	"context"
+	"io/ioutil"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -15,6 +20,8 @@ const (
 
 //ImageRepository to manage the db access
 type ImageRepository interface {
+	AddRawImage(originalFile []byte, filename string) (int, error)
+	GetRawImage(filename string) (int64, error)
 }
 
 //ImageDao to operate with the image objects
@@ -34,4 +41,65 @@ func InitiateImageDao(url string) (*ImageDao, error) {
 	}
 	dao := ImageDao{mongoClient: client}
 	return &dao, nil
+}
+
+//AddRawImage new raw image to the database
+func (dao *ImageDao) AddRawImage(originalFile []byte, filename string) (int, error) {
+	bucket, err := gridfs.NewBucket(
+		dao.mongoClient.Database(rawDatabase),
+	)
+
+	//error opening database connection
+	if err != nil {
+		return 0, err
+	}
+	uploadStream, err := bucket.OpenUploadStream(
+		filename,
+	)
+	//error opening upload stream
+	if err != nil {
+		return 0, nil
+	}
+	defer uploadStream.Close()
+
+	fileSize, err := uploadStream.Write(originalFile)
+	//error writing stream
+	if err != nil {
+		return 0, err
+	}
+	return fileSize, nil
+}
+
+//GetRawImage test
+func (dao *ImageDao) GetRawImage(filename string) (int64, error) {
+
+	db := dao.mongoClient.Database(rawDatabase)
+	fsFiles := db.Collection(filesCollection)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	var results bson.M
+	err := fsFiles.FindOne(ctx, bson.M{}).Decode(&results)
+	//error decoding results
+	if err != nil {
+		return 0, err
+	}
+
+	bucket, _ := gridfs.NewBucket(
+		db,
+	)
+	var buf bytes.Buffer
+	dStream, err := bucket.DownloadToStreamByName(filename, &buf)
+	//error downloading result
+	if err != nil {
+		return 0, err
+	}
+
+	err = ioutil.WriteFile(filename, buf.Bytes(), 0777)
+	//error writing file
+	if err != nil {
+		return 0, err
+	}
+	//alles gut
+	return dStream, nil
 }

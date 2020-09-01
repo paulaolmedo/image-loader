525e0854be416d2345cc7656f9db44302bd59265
paulaolmedo <paulabeatrizolmedo@gmail.com>
2020-08-31 21:21:26 -0300

    :zap: added new datatypes to the models for the database

--- mongo/models.go
+++ mongo/models.go
@@ -8,12 +8,31 @@ import (
 const (
 	//images  = "satellite_images"
 	idField = "_id"
+	db      = "safo"
 )
 
-//SatelliteImageDetails represents the satellite image itself
-type SatelliteImageDetails struct {
-	ID                           string `bson:"_id"`
+// SessionState type
+type SessionState string
+
+// SessionState values
+const (
+	Pending     SessionState = "PENDING"
+	Active      SessionState = "ACTIVE"
+	Blocked     SessionState = "BLOCKED"
+	Invalidated SessionState = "INVALIDATED"
+)
+
+//RawSatelliteImageDetails represents the satellite image itself
+type RawSatelliteImageDetails struct {
+	ID       string    `bson:"_id"`
+	fileName string    `bson:"file_name"`
+	dateTime time.Time `bson:"date_time"`
+}
+
+//ProcessedSatelliteImageDetails represents the satellite image already processed by the python service
+type ProcessedSatelliteImageDetails struct {
+	ID                           string    `bson:"_id"`
+	dateTime                     time.Time `bson:"date_time"`
 	images.GeographicInformation `bson:"geographic_information"`
 	images.NormalizedIndexes     `bson:"normalized_indexes"`
-	dateTime                     time.Time `bson:"date_time"`
 }

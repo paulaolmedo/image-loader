b054b97acdf8ac416354e0b421fa9649eae1a323
paulaolmedo <paulabeatrizolmedo@gmail.com>
2022-02-05 15:51:32 -0300

    fix tests

--- test/endpoints_test.go
+++ test/endpoints_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const mongoURL = "mongodb://localhost:27017"
+const mongoURL = "mongodb://mongo"
 
 var (
 	testserver        *httptest.Server
